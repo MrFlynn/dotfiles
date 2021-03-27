59a8b737d68e7f3c9eac2791552946504cb92d41
MrFlynn <nick@pleatsikas.me>
2021-03-27 16:59:54 -0700

    Fixed a couple of functionality bugs with install script.

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ set -ou pipefail
 if [[ "$(uname -s)" == Linux ]]; then
     DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
     
-    if [[ "$DISTRO" == "Ubuntu" ]]; then
-        apt-get install -y zsh fzf ripgrep git
+    if [[ "$DISTRO" == "\"Ubuntu\"" ]]; then
+        apt-get install -y zsh fzf ripgrep git curl
     else
         >&2 echo "OS not supported!"
         exit 1
@@ -33,11 +33,11 @@ chsh -s "$(command -v zsh)"
 find . -type f -iname ".*" -not -name ".gitignore" -exec mv -t ~ {} +
 
 # Install vim-plug.
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install vim packages.
-vim +PlugInstall
+vim +qall +PlugInstall
 
 # Configure git.
 git config --global user.email "nick@pleatsikas.me"
@@ -67,18 +67,19 @@ EOF
 }
 
 # (Optional) Create ssh key.
+printf "Do you wish to set up git clone over SSH? "
 while read -r option; do
     case $option in
         yes | y)
             genssh
-            exit            
+            break
             ;;
         no | n)
-            exit
+            break
             ;;
         *)
             >&2 echo "Unknown option"
-            ;;
     esac
 done
 
+echo "Done"
