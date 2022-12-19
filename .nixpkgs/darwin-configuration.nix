fcac2e2ea557e67e92a843d32ce501eaa8c3922c
Nick Pleatsikas <nick@pleatsikas.me>
2022-12-18 18:10:15 -0800

    Added darwnin NixOS configuration.

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+let
+  dontCheckPackage = drv: drv.overridePythonAttrs (old: { doCheck = false; });
+  customized-python = pkgs.python311.withPackages(p: with p; [
+    (dontCheckPackage httpie)
+  ]);
+in
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [ 
+    vim 
+    htop
+    fzf
+    ripgrep
+    tmux
+    go
+    jq
+    trash-cli
+    customized-python
+  ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
