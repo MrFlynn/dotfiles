28b54ead9f91cda1e5ad9fced31bfa01dd9fe818
Nick Pleatsikas <nick@pleatsikas.me>
2026-01-28 14:04:24 -0800

    Use official mac os icon
    Icon adapts better to tinted scheme on macOS 26

--- home.nix
+++ home.nix
@@ -161,7 +161,7 @@ in
     settings = {
       cursor-style = "block";
       font-size = 16;
-      macos-icon = "glass";
+      macos-icon = "official";
       macos-titlebar-style = "tabs";
       mouse-hide-while-typing = true;
       shell-integration-features = "no-cursor,ssh-terminfo";
